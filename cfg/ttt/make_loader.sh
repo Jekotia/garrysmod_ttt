5841f953b4b8836b144659a072259a19c770af4a
Josh 'Jekotia' Ameli <contact@jekotia.net>
2024-03-27 17:42:40 +0000

    Make make_loader.sh shellcheck compliant & execute. Add updated loader.cfg

--- cfg/ttt/make_loader.sh
+++ cfg/ttt/make_loader.sh
@@ -2,26 +2,26 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 loader_cfg=${SCRIPT_DIR}/loader.cfg
 
-rm -f ${loader_cfg}
-touch ${loader_cfg}
+rm -f "${loader_cfg}"
+touch "${loader_cfg}"
 
-echo "// Core" >> ${loader_cfg}
+echo "// Core" >> "${loader_cfg}"
 for f in core/*.cfg ; do
-	echo "exec ttt/${f%.*}" >> ${loader_cfg}
+	echo "exec ttt/${f%.*}" >> "${loader_cfg}"
 done
 
-echo "// Maps" >> ${loader_cfg}
+echo "// Maps" >> "${loader_cfg}"
 for f in maps/*.cfg ; do
-	echo "exec ttt/${f%.*}" >> ${loader_cfg}
+	echo "exec ttt/${f%.*}" >> "${loader_cfg}"
 done
 
-echo "// Randomats" >> ${loader_cfg}
+echo "// Randomats" >> "${loader_cfg}"
 for f in randomats/*.cfg ; do
-	echo "exec ttt/${f%.*}" >> ${loader_cfg}
+	echo "exec ttt/${f%.*}" >> "${loader_cfg}"
 done
 
-echo "// Roles" >> ${loader_cfg}
+echo "// Roles" >> "${loader_cfg}"
 for f in roles/*/*.cfg ; do
-	echo "exec ttt/${f%.*}" >> ${loader_cfg}
+	echo "exec ttt/${f%.*}" >> "${loader_cfg}"
 done
 
